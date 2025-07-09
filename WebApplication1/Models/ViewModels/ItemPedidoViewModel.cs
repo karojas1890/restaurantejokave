7f1f2e452666608a773c86c34313ef235c7a90a0
karojas1890 <katherine.rojas.salazar@cuc.cr>
2025-07-08 23:51:25 -0600

    La vista de ordenar interactua con la bd, trae los productos

---
+++ WebApplication1/Models/ViewModels/ItemPedidoViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Models.ViewModels
+{
+    public class ItemPedidoViewModel
+    {
+        public int IdProducto { get; set; }
+        public string Nombre { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+        public decimal PrecioTotal { get; set; }
+    }
+}
