879920fa4a9a6726919f96bf6b7001db81ed97c8
karojas1890 <katherine.rojas.salazar@cuc.cr>
2025-07-14 22:22:32 -0600

    prueba

--- WebApplication1/Models/ApplicationDbContext.cs
+++ WebApplication1/Models/ApplicationDbContext.cs
@@ -54,7 +54,7 @@ public partial class ApplicationDbContext : DbContext
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
     public virtual DbSet<Visita> Visita { get; set; }
-
+//
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Server=tiusr24pl.cuc-carrera-ti.ac.cr;Database=tiusr24pl_jokave;User id=usuariojokave;Password=jokave2025;Encrypt=False;");
